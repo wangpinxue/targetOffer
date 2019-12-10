1652c647efceff5903196b4b775fd00ab534041e
wangpinxue <345737143@qq.com>
2019-12-10 22:05:51 +0800

    开始写 将二叉搜索树转换成双链表

---
+++ src/com/wpx/convertTree/TreeNode.java
@@ -0,0 +1,32 @@
+package com.wpx.convertTree;
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public static TreeNode buildTree(int val) {
+        TreeNode root = new TreeNode(val);
+        TreeNode p = root;
+        for (int i = val - 1; i > 0; i--) {
+            p.left = new TreeNode(i);
+            if (--i > 0) {
+                p.right = new TreeNode(i);
+            }
+            p = p.right;
+        }
+        return root;
+    }
+
+    public static void printTree(TreeNode root){
+        if(root!=null){
+            printTree(root.left);
+            System.out.print(" " + root.val);
+            printTree(root.right);
+        }
+    }
+}
